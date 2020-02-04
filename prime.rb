5ad162c7017041ae6ab8dda33bccd0966df9a7e9
Seth Todd <seth.todd@outlook.com>
2020-02-04 03:28:39 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(number)
+  if number <= 1
+    return false
+  elsif number == 2
+  return true
+else
+  (2..Math.sqrt(number)).none? do |factors|
+    number % factors == 0
+  end
+end
+end
