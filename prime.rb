b220fca3c4f84341b1fdb216dbcfe5100b53dd0d
Seth Todd <seth.todd@outlook.com>
2020-02-04 03:24:46 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,9 @@
 def prime?(number)
   if number <= 1
     return false
-  else
+  elsif number = 2
+  return true
+else
   (2..Math.sqrt(number)).none? do |factors|
     number % factors == 0
   end
